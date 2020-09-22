7476158d09b962df61dd61d329218c1f53b6c8be
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-09-22 19:31:01 -0300

    282a bit++

---
+++ rust/src/282a.rs
@@ -0,0 +1,17 @@
+fn read_str() -> String {
+    let mut buffer: String = String::new();
+    std::io::stdin().read_line(&mut buffer).expect("Error");
+    buffer.trim().to_string()
+}
+
+fn read_int() -> u8 { read_str().parse().expect("Error") }
+
+fn main() {
+    let mut x: i16 = 0;
+    for _ in 0..read_int() {
+        let instruction: String = read_str();
+        if instruction.chars().nth(1) == Some('+') { x += 1; }
+        else { x -= 1; }
+    }
+    println!("{}", x);
+}
