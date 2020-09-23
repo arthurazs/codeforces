1ca1300725543ca7f2c6fa86c27330b58dbd5761
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-09-22 23:02:11 -0300

    improve 282a bit++

--- rust/src/282a.rs
+++ rust/src/282a.rs
@@ -10,7 +10,7 @@ fn main() {
     let mut x: i16 = 0;
     for _ in 0..read_int() {
         let instruction: String = read_str();
-        if instruction.chars().nth(1) == Some('+') { x += 1; }
+        if &instruction[1..2] == "+" { x += 1; }
         else { x -= 1; }
     }
     println!("{}", x);
