ec2f3f5c3a3f4a53d1a11fbc10f8517a2966f9e3
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-09-27 22:26:00 -0300

    58a chat room

---
+++ rust/src/58a.rs
@@ -0,0 +1,20 @@
+mod utils;
+use utils::read_str;
+
+fn main() {
+    let s: String = read_str();
+    let mut found: usize = 0;
+    let word: &str = "hello";
+
+    for letter in s.chars() {
+        if word.chars().nth(found).expect("Error") == letter {
+            found += 1;
+            if found == 5 {
+                println!("YES");
+                std::process::exit(0);
+            }
+        }
+    }
+    println!("NO");
+
+}
