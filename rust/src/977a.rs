9103944d445cfd41765a3fc0a75c6dd45c93b0ca
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-09-28 21:54:18 -0300

    977a wrong subtraction

---
+++ rust/src/977a.rs
@@ -0,0 +1,16 @@
+mod utils;
+use utils::read_vec;
+
+fn main() {
+    let (mut number, times): (u32, u8) = {
+        let values: Vec<u32> = read_vec(' ');
+        (values[0], values[1] as u8)
+    };
+
+    for _ in 0..times {
+        if number % 10 == 0 { number /= 10; }
+        else { number -= 1; }
+    }
+
+    println!("{}", number);
+}
