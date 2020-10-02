932dec79bd0b2644c05164951e13ed8b10524684
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-10-01 23:40:37 -0300

    41a translation

---
+++ rust/src/41a.rs
@@ -0,0 +1,8 @@
+mod utils;
+use utils::read_str;
+
+fn main() {
+    let (s, t): (String, String) = (read_str(), read_str());
+    if s.chars().rev().collect::<String>() == t { println!("YES"); }
+    else { println!("NO"); }
+}
