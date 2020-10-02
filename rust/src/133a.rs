02745a31d7008153fd51c25e6b60e2975d77d51d
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-10-02 16:45:41 -0300

    133a hq9+

---
+++ rust/src/133a.rs
@@ -0,0 +1,12 @@
+mod utils;
+use utils::read_str;
+
+fn main() {
+    for instruction in read_str().chars() {
+        if "HQ9".contains(instruction) {
+            println!("YES");
+            std::process::exit(0);
+        }
+    }
+    println!("NO");
+}
