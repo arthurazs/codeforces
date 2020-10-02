50fbb9f6f06d9820a03dab5a4234bb8bb31eaa81
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-10-01 22:15:01 -0300

    110a nearly lucky number

---
+++ rust/src/110a.rs
@@ -0,0 +1,13 @@
+mod utils;
+use utils::read_str;
+
+fn main() {
+    let mut lucky_digits: u8 = 0;
+
+    for digit in read_str().chars() {
+        if digit == '4' || digit == '7' { lucky_digits += 1; }
+    }
+
+    if lucky_digits == 4 || lucky_digits == 7 { println!("YES"); }
+    else { println!("NO"); }
+}
