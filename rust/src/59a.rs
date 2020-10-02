41f46c33e36a03650141ef2902a917c61bcb17fb
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-10-01 23:12:19 -0300

    59a word

---
+++ rust/src/59a.rs
@@ -0,0 +1,17 @@
+mod utils;
+use utils::read_str;
+
+fn main() {
+    let word: String = read_str();
+
+    let mut lower: i8 = 0;
+
+    for letter in word.chars() {
+        if letter.is_lowercase() { lower += 1; }
+        else { lower -= 1; }
+    }
+
+    if lower >= 0 { println!("{}", word.to_lowercase()); }
+    else { println!("{}", word.to_uppercase()); }
+
+}
