d55dc44eafee246af216814d229006ab1c30b183
Arthur Zopellaro <arthurazsoares@gmail.com>
2020-10-14 12:39:23 -0300

    improve 318a

--- rust/src/a/318.rs
+++ rust/src/a/318.rs
@@ -1,10 +1,8 @@
-use utils::read_tuple;
+use utils::{read_tuple, Utils};
 
 fn main() {
     let (k, n): (u64, u64) = read_tuple();
-    let mut mid: u64 = k / 2;
-
-    if k % 2 == 1 { mid += 1; }
+    let mid: u64 = k.div_ceil(2);
 
     if n > mid { println!("{}", 2 * (n - mid)); }
     else { println!("{}", (2 * n) - 1); }
